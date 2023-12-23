8980edd6da6d0cbbc84d5ac9562e475a0749a012
jhalljhall <hall.justin@justinhall.com>
2023-12-23 13:26:36 -0500

    adding ai controller, endpoint

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,14 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import login, users, utils, businesses, drinks, tags, reviews, collections
+from app.api.api_v1.endpoints import login, users, utils, ai
 #collections
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-api_router.include_router(businesses.router, prefix="/businesses", tags=["businesses"])
-api_router.include_router(drinks.router, prefix="/drinks", tags=["drinks"])
-api_router.include_router(collections.router, prefix="/collections", tags=["collections"])
-api_router.include_router(reviews.router, prefix="/reviews", tags=["reviews"])
-api_router.include_router(tags.router, prefix="/tags", tags=["tags"])
+api_router.include_router(ai.router, prefix="/ai", tags=["ai"])
--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,2 +1,3 @@
 from .UserController import user
 from .TokenController import token
+from .AIController import ai
