266d757e896024fbfb29a934413876a11a87fa17
Justin Hall <hall.justin@justinhall.com>
2023-12-24 02:21:08 +0000

    debugging OpenAI

--- app/controllers/AIController.py
+++ app/controllers/AIController.py
@@ -14,53 +14,37 @@ import io
 class AIController():
     async def describe_image(self, db: Session, image_url: str) -> str:
         client = OpenAI()
-        print(f"image: {image_url}")
         try:
             response = client.chat.completions.create(
-            model="gpt-4-vision-preview",
-            messages=[
+                model="gpt-4-vision-preview",
+                messages=[
                 {
-                "role": "user",
-                "content": [
-                    {
-                    "type": "text",
-                    "text": "What is in this image?",
-                    },
-                    {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": image_url,
-                    },
-                    },
-                ],
+                    "role": "user",
+                    "content": [
+                        {
+                        "type": "text",
+                        "text": "What is in this image?",
+                        },
+                        {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": image_url,
+                        },
+                        },
+                    ],
                 }
             ],
             max_tokens=500,
             )
             return {"msg": response.choices[0].message.content}
-            # Handling different HTTP responses
-            if response.status_code == 200:
-                return {"msg": response.choices[0].message.content}
-            elif 400 <= response.status_code < 500:
-                return {"msg": "Client error occurred."}
-            elif 500 <= response.status_code < 600:
-                return {"msg": "Server error occurred."}
-            else:
-                return {"msg": "Unexpected error occurred."}
-
-        except openai.APIError as e:
-            #Handle API error here, e.g. retry or log
-            print(f"OpenAI API returned an API Error: {e}")
-            return {"msg": f"OpenAI API returned an API Error: {e}"}
             
+        except openai.APIError as e:
+            raise HTTPException(status_code=400, detail="Could not get the public url of the image")
+
         except openai.APIConnectionError as e:
-            #Handle connection error here
-            print(f"Failed to connect to OpenAI API: {e}")
-            return {"msg": f"Failed to connect to OpenAI API: {e}"}
+            raise HTTPException(status_code=500, detail="Could not connect to the OpenAI API")
             
         except openai.RateLimitError as e:
-            #Handle rate limit error (we recommend using exponential backoff)
-            print(f"OpenAI API request exceeded rate limit: {e}")
             return {"msg": f"OpenAI API request exceeded rate limit: {e}"}
             
         return {"msg":"Unexpected error occurred."}
